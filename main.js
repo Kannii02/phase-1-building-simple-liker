a753232f2ce7c0cdb520f348f4e2c494032537ea
kannikarsriburin <i_m_juy-juys@hotmail.co.th>
2025-01-17 15:49:24 -1000

    phase-1-building-simple-liker

--- main.js
+++ main.js
@@ -3,9 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll(".like-glyph");
 
+hearts.forEach(heart => {
+  heart.addEventListener("click", () => {
+    handleLike(heart);
+  });
+});
 
+function handleLike(heart) {
+  mimicServerCall()
+    .then(() => {
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.classList.add("activated-heart");
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove("activated-heart");
+      }
+    })
+    .catch(error => {
+      displayError(error);
+    });
+}
 
+function displayError(error) {
+  const modal = document.getElementById("modal");
+  const modalMessage = document.getElementById("modal-message");
+
+  modalMessage.innerText = error;
+  modal.classList.remove("hidden"); // Show error modal
+
+  setTimeout(() => {
+    modal.classList.add("hidden"); // Hide after 3 seconds
+  }, 3000);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
